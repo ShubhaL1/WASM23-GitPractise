496f6da9e7804916dbced63aeb2c85d93c27f22b
shubh <shubh@LAPTOP-ST80I3H1>
2023-01-17 11:50:11 +0530

    2nd Commit-17012023

---
+++ src/test/java/demo/CreateContactTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest 
+{
+	@Test
+	public void createContact()
+	{
+		System.out.println("Create Contact");
+	}
+
+}
